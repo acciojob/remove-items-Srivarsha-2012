29296def042b0eb5ce9fedbf98c6aa99cbefba1e
srivarsha <algotesrivarsha@gmail.com>
2026-04-07 22:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function removeColor() {
+    // Get the dropdown element
+    let select = document.getElementById("colorSelect");
+
+    // Get the selected index
+    let selectedIndex = select.selectedIndex;
+
+    // Remove the selected option
+    if (selectedIndex !== -1) {
+        select.remove(selectedIndex);
+    }
+}
